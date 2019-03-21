69b9339d0a396f52e221a2fb4e8cba2aa960bffb
Jantakarn12 <khimmyparty@gmail.com>
2019-03-22 01:02:10 +0700

    create register page(demo)

--- Components/Register.js
+++ Components/Register.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Image } from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid'
+import { Container, Header, Content, Form, Item, Input,Button,Label,Left,Right,Body,Title,Text } from 'native-base';
 
 export default class Register extends Component {
   constructor(props) {
@@ -10,9 +12,46 @@ export default class Register extends Component {
 
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Container>
+        <Header renderHeader = "center" backgroundColor = "#2cba2c" >
+            <Left/>
+              <Body>
+                <Title>Register</Title>
+              </Body>
+            <Right />
+        </Header>
+
+
+
+
+        <Content>
+          <Form>
+              <Item floatingLabel>
+                <Label>Firstname</Label>
+                <Input />
+              </Item>
+
+              <Item floatingLabel>
+                <Label>Lastname</Label>
+                <Input />
+              </Item>
+
+              <Item floatingLabel>
+                <Label>Username</Label>
+                <Input />
+              </Item>
+
+              <Item floatingLabel>
+                <Label>Password</Label>
+                <Input />
+              </Item>
+
+              <Button style={{alignSelf:'center',marginTop:10}} primary><Text> Sign up </Text></Button>
+            </Form>
+
+
+        </Content>
+      </Container>
     );
   }
 }
